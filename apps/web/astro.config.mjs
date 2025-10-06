28ed3ebbf9e6b131d9de63291cb0339c93492d05
Cristopher Valencia <kulashaker30@gmail.com>
2025-10-06 17:20:05 +0800

    Fixing prisma related issue when deploying to railway

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,10 +3,10 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { fileURLToPath } from 'node:url';
-
+import vercel from '@astrojs/vercel/serverless'; // Vercel adapter
 export default defineConfig({
   output: 'server',
-  adapter: node({ mode: 'standalone' }),
+  adapter: vercel(),
   integrations: [react(), tailwind()],
   vite: {
     resolve: {
