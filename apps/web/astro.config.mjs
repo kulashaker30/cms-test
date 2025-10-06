7611058bcaafa9db416c510085a28f1802d6dbe5
Cristopher Valencia <kulashaker30@gmail.com>
2025-10-06 18:59:11 +0800

    Moving the MetricCard component to apps/web

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { fileURLToPath } from 'node:url';
 import vercel from '@astrojs/vercel/serverless';
