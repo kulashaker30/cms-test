565d0e6c6bb4140eaa0ee17c1cfe48e33aa4b216
Cristopher Valencia <kulashaker30@gmail.com>
2025-10-06 18:32:49 +0800

    Apply .gitignore rules and remove previously tracked ignored files

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,7 +3,7 @@ import react from '@astrojs/react';
 import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import { fileURLToPath } from 'node:url';
-import vercel from '@astrojs/vercel/serverless'; // Vercel adapter
+import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
